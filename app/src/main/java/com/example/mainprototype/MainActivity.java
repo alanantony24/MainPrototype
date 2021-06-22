90467cc7ae5d9401bb63ca0ac5d3878916cf0bbc
alanantony24 <s10207327@connect.np.edu.sg>
2021-06-22 20:28:41 +0800

    Revert "Fixed Errors"
    This reverts commit ccbe8d7f

--- app/src/main/java/com/example/mainprototype/MainActivity.java
+++ app/src/main/java/com/example/mainprototype/MainActivity.java
@@ -1,26 +1,20 @@
 package com.example.mainprototype;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         Button startRun = findViewById(R.id.startRun);
         startRun.setOnClickListener(new View.OnClickListener() {
@@ -36,18 +30,4 @@ public class MainActivity extends AppCompatActivity {
         intent.setAction(action);
         MainActivity.this.startService(intent);
     }
-    private void requestPermission(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            if(getApplicationContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(this, "Permission granted !", Toast.LENGTH_SHORT).show();
-            }
-            else{
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.FOREGROUND_SERVICE}, 1);
-            }
-        }else{
-            Toast.makeText(this, "Permission granted !", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
 }
